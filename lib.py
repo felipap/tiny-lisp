f415f5c48036452a414c8b86a205630121b9cdb0
f03lipe <pires.a.felipe@gmail.com>
2013-04-02 14:37:53 -0300

    Feature added: define

--- lib.py
+++ lib.py
@@ -4,9 +4,12 @@
 from functools import reduce
 
 lib = {
-	'*': lambda a, *b: a*reduce(lambda x,y:x*y, b) or 3, # to make sure more than one
-	'+': lambda a, *b: a+reduce(lambda x,y:x+y, b) or 3, # argument is passed.
-	'=': lambda a, *b: all(a == e for e in b) or 3,
+	'*': lambda a, *b: a*reduce(lambda x,y:x*y, b), # to make sure more than one
+	'+': lambda a, *b: a+reduce(lambda x,y:x+y, b), # argument is passed.
+	'=': lambda a, *b: all(a == e for e in b),
+	'-': lambda a, b: a-b,
+	'/': lambda a, b: a/b,
+	'%': lambda a, b: a%b,
 	
 	'list': lambda *els: els,
 	'map': lambda proc, array: [proc(e) for e in array],
--- python.py
+++ python.py
@@ -9,24 +9,20 @@ def tokenize(code): # What? You don't know what a tokenizer is?
 		while i < len(prog): # Reading (i) starts where parent call left.
 			i += 1
 			if prog[i] == ')': break # Bracket from parent level: return.
-			if prog[i:i+2] == ['(',')']: # empty list = nil
+			elif prog[i:i+2] == ['(',')']: # empty list = nil
 				d = 'nil'
-			elif prog[i] == '(': 
-				if prog[i+2] == ')': # Single element within brackets:
-					d = prog[i+1] # don't nest: get element
-					i += 2 # and jump brackets.
-				else: # A nested expression.
-					d, i = nest(prog, i) # Get content (d) and update i.
+			elif prog[i] == '(': # Nested expression:
+				d, i = nest(prog, i) # get content (d) and update the index.
 			else: d = prog[i] # Else, just a symbol.
 			array.append(d)
 		return array, i # Tell parent call how far your reading went.
 
 	import re
-	tokens = re.split('(\(|\)|\s+|".*?[^\\\\])', code)
+	tokens = re.split('(\(|\)|\s+|".*?[^\\\\]")', code)
 	return nest([c for c in tokens if c and not c.isspace()])[0] # Ignore blank.
 
 def proc(argn, proc, env):
-	# print("making procedure (%s) (%s)" % (argn, proc), argn)
+	# print("creating procedure with args:{%s}, body:{%s}." % (argn, proc), argn)
 	def wrapper(*args):
 		nenv = env.copy()
 		for i in range(len(argn)):
@@ -34,29 +30,40 @@ def proc(argn, proc, env):
 		return eval(proc, nenv)
 	return wrapper
 
-listify = lambda x: [x] if not type(x)==list else x # Turns to list what isn't.
+listify = lambda x: [x] if not type(x)==list else x # Turns into list what isn't
+unlistify = lambda x: x[0] if type(x)==list and len(x)==1 else x
 	
 def eval(exp, env):
-	# print("eval", exp, env.keys())
-	if isinstance(exp, list): # If expression is a list
+	# print("eval(%s) >> %s" % (exp, env))
+	exp = unlistify(exp)
+	if isinstance(exp, list): # If exp is a list
 		if isinstance(exp[0], str): # which starts with a string:
 			if exp[0].startswith('cond'): # It's a condition statement
 				for cond, retv in exp[1:]: # => loop through conditions.
 					if eval(cond, env):
-						return retv
-				return None # Nothing evaluated to True, return None.
-			elif exp[0].startswith('lambda') or exp[0] == 'λ': # or a lambda.
+						return eval(retv, env)
+				return 3.1415 # Nothing evaluated to True, return Pie.
+			elif exp[0] in ('lambda', 'λ'): # or a lambda.
 				return proc(listify(exp[1]), exp[2], env) # => make procedure.
+		elif exp[0][0] == 'define': # It's a define statement!
+			nenv = env.copy() # Create new environment for nested expressions.
+			for i in range(len(exp)): # Loop through (define ...) statements.
+				if exp[i][0] != 'define': break # Exp to return was found
+				else:
+					nenv[exp[i][1]] = eval(exp[i][2], nenv)
+			return eval(exp[-1], nenv) # Eval expression to return.
 		# Not lambda nor cond: it's a procedure! Eval proc and args, then apply!
 		return apply(eval(exp[0], env), [eval(i,env) for i in exp[1:]], env)
-	else: # Expressipon is not a list: it's a constant.
-		try: # Duck typing to check if it's a number.
-			return float(exp) # Use float for all numbers. Hex not allowed.
+	else: # exp isn't a list: it's a constant.
+		try: # Check if it's a number.
+			return float(exp) # Use float for everything. Hex not allowed (yet?)
 		except: pass
 		if exp[0] in ('\'', '\"'): # Quoted name.
-			return exp
+			return exp.replace('\'', '')
 		else: # It's a symbol => lookup it up.
 			# print("Looking up %s in %s" % (exp, env.keys()))
+			if not env.get(exp):
+	 			return lib[exp]
 			return env[exp]
 
 def apply(proc, args, env): # Apply the procedure to evaluated args.
@@ -64,8 +71,8 @@ def apply(proc, args, env): # Apply the procedure to evaluated args.
 	return proc(*args)
 
 if __name__ == "__main__":
-	s = input('>> ') or '(list "2 3)' or '((λ x (+ x 2)) 3)'
+	s = input('>> ') or '((define pow (lambda (x n) (cond ((= n 1) x) (else (* '\
+	'x (pow x (- n 1))))))) (pow 2 3))' 
 
 	from lib import lib
-	print(tokenize(s))
-	print(eval(tokenize(s), lib.copy()))
+	print(eval(tokenize(s), dict()))
